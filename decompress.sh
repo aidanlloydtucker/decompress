b6293e7de07a0a80b9d624bebd066a78b9c800c2
billybobjoeaglt <aidanlloydtucker@gmail.com>
2014-11-15 18:44:43 -0800

    Added else. Added unzip.

--- decompress.sh
+++ decompress.sh
@@ -2,15 +2,23 @@
 FILE1=$(file -b $1)
 if [[ $FILE1 == *Zip* ]]; then
     echo "Zip"
-fi
-if [[ $FILE1 == *tar* ]]; then
-    echo "Tar"
-fi
-if [[ $FILE1 == *gzip* ]]; then
-    echo "Tar.gz"
-fi
-if [[ $FILE1 == *bzip2* ]]; then
-    echo "Tar.bz2"
+    unzip $1
+
 else
-    echo "WTF"
+    if [[ $FILE1 == *tar* ]]; then
+        echo "Tar"
+
+    else
+        if [[ $FILE1 == *gzip* ]]; then
+            echo "Tar.gz"
+
+        else
+            if [[ $FILE1 == *bzip2* ]]; then
+                echo "Tar.bz2"
+
+                else
+                    echo "WTF"
+            fi
+        fi
+    fi
 fi
