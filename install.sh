cf09733da32c3fa2a464fb3b2251c82654ea61be
billybobjoeaglt <aidanlloydtucker@gmail.com>
2014-11-18 15:44:10 -0800

    Added OS support

--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
+
+
 #!/bin/bash
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-   platform='linux'
-   sudo cp decompress /usr/bin
-   sudo cp dcmp /usr/bin
-   sudo apt-get install unzip tar p7zip p7zip-full
-elif [[ "$unamestr" == 'Darwin' ]]; then
-   platform='MacOSX'
-   sudo cp decompress /usr/bin
-   sudo cp dcmp /usr/bin
+unamestr=$(uname)
+opsys=$(lsb_release -a)
+if [[ $unamestr == *Linux* ]]; then
+    if [[ $opsys == *Debian* ]]; then
+        #sudo cp decompress /usr/bin
+        #sudo cp dcmp /usr/bin
+        #sudo apt-get install unzip tar p7zip p7zip-full
+	echo "Deb"
+    elif [[ $opsys == *Ubuntu* ]]; then
+        #sudo cp decompress /usr/bin
+        #sudo cp dcmp /usr/bin
+        #sudo apt-get install unzip tar p7zip p7zip-full
+	echo "Ubuntu"
+    else
+	echo "I know you have Linux, but what type"
+    fi
+elif [[ $unamestr == *Darwin* ]]; then
+    #sudo cp decompress /usr/bin
+    #sudo cp dcmp /usr/bin
+    echo "Mac"
+else
+    echo "Your OS is not supported."
+    exit
 fi
 echo ""
 echo "----------------------"
