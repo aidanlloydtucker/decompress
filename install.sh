27e2143c706443a66bbfed55e2c16b52141fe463
billybobjoeaglt <aidanlloydtucker@gmail.com>
2014-11-18 11:09:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-sudo apt-get install unzip tar p7zip p7zip-full
-sudo cp decompress /usr/bin
-sudo cp dcmp /usr/bin
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+   sudo cp decompress /usr/bin
+   sudo cp dcmp /usr/bin
+   sudo apt-get install unzip tar p7zip p7zip-full
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='MacOSX'
+   sudo cp decompress /usr/bin
+   sudo cp dcmp /usr/bin
+fi
 echo ""
 echo "----------------------"
 echo "This is now installed!"
